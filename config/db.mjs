defc721b566675503757f4d4ed06e05bb6f02b8a
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-01-27 11:37:55 -0500

    create/update: created db connection

---
+++ config/db.mjs
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import {logger} from '../middleware/logger.mjs';
+
+const conn = async () => {
+  try {
+    const {connection} = await mongoose.connect(process.env.MONGO_URI);
+    logger.info(`MongoDb Connection ${connection.host} Successful.`);
+  }
+  catch (e) {
+    logger.error(`MongoDb Connection Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+export default conn;
