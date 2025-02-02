9b5011da565f1b2baf78cd890f3e54ca39ff66fa
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-02-02 14:10:52 -0500

    created/updated: updated cors

--- server.mjs
+++ server.mjs
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT;
 conn();
 
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: 'http://localhost:5173', credentials: true, }))
 app.use(express.json());
 
 const MaleProfileRoutes = await import('./routes/maleProfileRoutes.mjs').then(
