5d7c83375b59d67d47839eba22e5369da705d8f0
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-02-03 15:08:11 -0500

    created/updated: updated model, added photo, profile image, timestamps

--- models/registration.mjs
+++ models/registration.mjs
@@ -1,29 +1,26 @@
 import mongoose from 'mongoose';
 
 const registrationSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: true
-  },
-  gender: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-})
+      name: {
+        type: String, required: true,
+      }, age: {
+        type: Number, required: true,
+      }, gender: {
+        type: String, required: true,
+      }, email: {
+        type: String, required: true, unique: true,
+      }, password: {
+        type: String, required: true,
+      }, photo: {
+        type: String,
+      }, profileImage: {
+        type: String
+      },
+    },
+    {timestamps: true},
+);
 
-const RegistrationModel = mongoose.model('RegistrationModel', registrationSchema);
+const RegistrationModel = mongoose.model('RegistrationModel',
+    registrationSchema);
 
 export default RegistrationModel;
