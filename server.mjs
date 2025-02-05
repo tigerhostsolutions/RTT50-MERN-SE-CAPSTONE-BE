05cedd5c0b7eae212d63ea37aafad5116826ce15
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-02-04 20:20:19 -0500

    created/updated: changed port backend port from 3000 to 5000

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ import Registration from './models/registration.mjs';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 conn();
 
 const allowedOrigins = [
