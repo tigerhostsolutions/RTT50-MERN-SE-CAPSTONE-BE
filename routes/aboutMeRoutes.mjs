80d421237aa759c185c6ce4251b1d6ba6635d177
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-02-06 00:47:19 -0500

    Continuing to develop, hoping things progress w/o failure in local and production. ---- Added about me. Issues in-progress, unable to save, token expiration. ---

--- routes/aboutMeRoutes.mjs
+++ routes/aboutMeRoutes.mjs
@@ -1,13 +1,16 @@
 import express from 'express';
 import AboutMe from '../models/about.mjs';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 const router = express.Router();
+dotenv.config();
 
 // Update Bio
-router.patch('/api/members/aboutMe', async (req, res) => {
+router.patch('/', async (req, res) => {
   const { bio } = req.body;
   const token = req.header('Authorization').replace('Bearer ', '');
-  const decoded = jwt.verify(token, 'your_secret_key');
+  // const decoded = jwt.verify(token, 'your_secret_key');
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
   const member = await AboutMe.findById(decoded.id);
   member.bio = bio;
